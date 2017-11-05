ba2d0d11ea6da7dc2051a7f97599894327a2de1b
Chao <wangchao0519@hotmail.com>
2017-11-05 21:32:51 +0800

    Part 1, Lesson 3, Docker Config

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,7 @@
 # project/__init__.py
 
 
+import os
 from flask import Flask, jsonify
 
 
@@ -8,8 +9,11 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
+print(app.config)
+# print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> hahaha')
 
 @app.route('/ping', methods=['GET'])
 def ping_pong():
