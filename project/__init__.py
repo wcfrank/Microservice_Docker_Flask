795dfdf54c11a5d8d13b949a5228dc8d7725533e
Chao <wangchao0519@hotmail.com>
2017-11-07 13:23:37 +0800

    Part 1, Lesson 4, Postgres Setup

--- project/__init__.py
+++ project/__init__.py
@@ -2,7 +2,9 @@
 
 
 import os
+import datetime
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 
 # instantiate the app
@@ -12,8 +14,25 @@ app = Flask(__name__)
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
 
-print(app.config)
-# print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> hahaha')
+# instantiate the db
+db = SQLAlchemy(app)
+
+# model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=False, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+        self.created_at = datetime.datetime.utcnow()
+
+
+# routes
 
 @app.route('/ping', methods=['GET'])
 def ping_pong():
