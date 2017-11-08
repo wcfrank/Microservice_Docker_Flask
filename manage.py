292b83f54f0558a65df4a53a5b13865ac4f25f71
Chao <wangchao0519@hotmail.com>
2017-11-08 10:52:07 +0800

    Part 1, Lesson 6, Flask Blueprints

--- manage.py
+++ manage.py
@@ -1,13 +1,26 @@
 # manage.py
 
 
-from flask_script import Manager
-
-from project import app, db
-
 import unittest
 
+from flask_script import Manager
+
+from project import create_app, db
+from project.api.models import User
+
+
+app = create_app()
 manager = Manager(app)
+
+
+@manager.command
+def test():
+    """Runs the unit tests without test coverage."""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 
 @manager.command
@@ -17,14 +30,6 @@ def recreate_db():
     db.create_all()
     db.session.commit()
 
-@manager.command
-def test():
-    """Runs the tests without code coverage."""
-    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        return 0
-    return 1
 
 if __name__ == '__main__':
     manager.run()
